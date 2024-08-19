622515f389afd8442d4b89e3eebe2a0dbbe723c1
fatma <faatmaaahmedd12@gmail.com>
2024-08-19 19:23:58 +0300

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flower_app/pages/checkout.dart';
 import 'package:flower_app/pages/details_screen.dart';
 import 'package:flower_app/pages/home.dart';
@@ -11,7 +12,13 @@ import 'package:flower_app/provider/cart.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
-void main() {
+
+import 'firebase_options.dart';
+void main() async {
+    /*WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );*/
   runApp(const MyApp());
 }
  
